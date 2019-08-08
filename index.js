1d4d1a0ade6715c922f7c88254f3c7f6f7d2b559
leroyg <leroy.gardner@gmail.com>
2019-08-07 20:31:19 -0500

    get all, get by ID working

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 // implement your API here
+const express = require('express');
+
+// other files
+const db = require('./data/db');
+
+
+//global objects
+const server = express();
+
+
+//middleware
+server.use(express.json());
+
+server.get('/', (req, res) => {
+    res.send('<h3>Hello from your GET call on Express</h3>');
+})
+
+//get all users
+server.get('/api/users', (req, res) =>{
+    db.find()
+    .then(users =>{
+        res.json(users);
+    })
+    .catch(error =>{
+        res.status(500).json({
+            error: error
+        })
+    })
+})
+
+// get users by specific ID
+server.get('/api/users/:id', (req, res) => {
+    const { id } = req.params;
+    db.findById(id)
+    .then( data => {
+        res.status(201).json(data)
+    })
+    .catch(error => {
+        res.status(500).json({
+            error: error
+        })
+    })
+})
+
+server.listen(port=(4000), ()=>{
+    console.log(`server is listening on port ${port}`);
+});
